e8844d973bfc6ac358c3c8b68e58d3b1b9512c31
chrialge <chrialge@gmail.com>
2024-04-18 15:09:52 +0200

    add exercise

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
 
+// creo una classe con il nome production
 class Production
 {
 
+    // creouna funzione di costrutto dove prendo i parametri e li inserisco dell'istanta con la sua key
 
     function __construct(public string $title, public string $language, public int $vote)
     {
@@ -12,13 +14,30 @@ class Production
     }
 }
 
-$hammer = new Production("Hammer", "eng", rand(1, 10));
-// $hammer->title = "Hammer";
-// $hammer->language = "eng";
-// $hammer->vote = rand(1, 10);
-echo $hammer->title;
-var_dump($hammer);
 
+/**
+ * creo una variabile che crea un istanza con il nome della varibile e gli passa tre valori che verrano poi assegnati all' istanza
+ */
+$hammer = new Production("Hammer", "eng", rand(1, 10));
+
+// richiamo il valore title nell'istanza hammer
+echo $hammer->title;
+// dumbo per vedere cosa ce dentro all'istanza di hammer
+var_dump($hammer);
+// inserisco il contenuto dell'istanza ad un array
+$array[] = $hammer;
+
+/**
+ * creo una variabile che crea un istanza con il nome della varibile e gli passa tre valori che verrano poi assegnati all' istanza
+ */
 $sword = new Production("sword", "it", rand(1, 10));
 
+// dumbo per vedere cosa che dentro l'istanza di sword
 var_dump($sword);
+// inserisco il contenuto dell'istanza all array
+$array[] = $sword;
+
+
+
+// dumbo il contenuto dell'array
+var_dump($array);
